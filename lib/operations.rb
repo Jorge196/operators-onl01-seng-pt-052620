30d9abd0e8b85e955603fc8a7d10792175730d91
Jorge Ramirez <joramirez96@gmail.com>
2020-06-04 23:43:28 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 def unsafe?(speed)
   if speed < 40 || speed > 60
     true
@@ -9,5 +9,5 @@ end
 
 
 def not_safe?(speed)
-  speed < 40 || speed > 60 ? true : false 
+  speed < 40 || speed > 60 ? true : false
 end
